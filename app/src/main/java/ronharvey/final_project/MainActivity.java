37a79e53085262c110bc2de6800ed45ba1568651
ron harvey <harveyrt3@gmail.com>
2017-11-02 16:32:53 -0500

    map api (broken)

--- app/src/main/java/ronharvey/final_project/MainActivity.java
+++ app/src/main/java/ronharvey/final_project/MainActivity.java
@@ -1,13 +1,32 @@
 package ronharvey.final_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btn_location = (Button) findViewById(R.id.btn_location);
+        btn_location.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+
+
+        switch (v.getId()) {
+
+            case R.id.btn_location:
+
+                Intent intent = new Intent(MainActivity.this, Location.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
