0e7bc0738fd6c552706d14e2008162c8ee92c356
nirushanm <sainirush@yahoo.com>
2017-11-20 12:26:12 -0600

    finished customize class...and intent in it

--- app/src/main/java/ronharvey/final_project/Customize.java
+++ app/src/main/java/ronharvey/final_project/Customize.java
@@ -21,7 +21,8 @@ public class Customize extends AppCompatActivity {
 
 
     String toppings="";
-    double topping_price=0.0;
+    String type ="";
+    double total_price=0.0;
     int counter=0;
     boolean boo_pepperoni = false;
     boolean boo_pineapple = false;
@@ -39,14 +40,24 @@ public class Customize extends AppCompatActivity {
         setContentView(R.layout.activity_customize);
 
         CheckBox pepperoni = (CheckBox)findViewById(R.id.chk_pepperoni);
+        pepperoni.setChecked(true);
+        boo_pepperoni=true;
         CheckBox pineapple = (CheckBox)findViewById(R.id.chk_pineapple);
+        pineapple.setChecked(true);
+        boo_pineapple=true;
         CheckBox peppers = (CheckBox)findViewById(R.id.chk_peppers);
+        boo_peppers=true;
+        peppers.setChecked(true);
         CheckBox onions = (CheckBox)findViewById(R.id.chk_onions);
+        onions.setChecked(true);
+        boo_onions=true;
         final ImageView v= (ImageView) findViewById(R.id.v1);
         final Button submit= (Button) findViewById(R.id.bttn_submit);
+        final Button cancel= (Button) findViewById(R.id.bttn_cancel);
+        final Button payment= (Button) findViewById(R.id.bttn_payment);
 
         Intent intent = getIntent();
-        String type = intent.getStringExtra("PIZZA_TYPE");
+        type = intent.getStringExtra("PIZZA_TYPE");
         String price = intent.getStringExtra("UNIT_PRICE");
          double unit_p=Double.parseDouble(price);
 
@@ -145,23 +156,98 @@ public class Customize extends AppCompatActivity {
             public void onClick(View view) {
 
                 if (boo_pepperoni == true && boo_onions == true && boo_pineapple == true && boo_peppers == true) {
-                    v.setImageResource(R.drawable.meat);
+                    v.setImageResource(R.drawable.all_toppings);
+                }
+
+                else if(boo_pepperoni == false && boo_onions == true && boo_pineapple == true && boo_peppers == true) {
+                    v.setImageResource(R.drawable.oni_pepper_pine);
                 }
                 else if (boo_pepperoni == true && boo_onions == false && boo_pineapple == true && boo_peppers == true) {
-                    v.setImageResource(R.drawable.bbq);
+                    v.setImageResource(R.drawable.pepperoni_pepp_pine);
                 }
                 else if (boo_pepperoni == true && boo_onions == true && boo_pineapple == false && boo_peppers == true) {
-                    v.setImageResource(R.drawable.cheese);
+                    v.setImageResource(R.drawable.pepperoni_oni_pepp);
                 }
                 else if (boo_pepperoni == true && boo_onions == true && boo_pineapple == true && boo_peppers == false) {
+                    v.setImageResource(R.drawable.pepperoni_oni_pine);
+                }
+
+                else if (boo_pepperoni == true && boo_onions == true && boo_pineapple == false && boo_peppers == false) {
+                    v.setImageResource(R.drawable.pepperoni_onion);
+                }
+
+                else if (boo_pepperoni == true && boo_onions == false && boo_pineapple == true&& boo_peppers == false) {
+                    v.setImageResource(R.drawable.pepperoni_pine);
+                }
+
+                else if (boo_pepperoni == true && boo_onions == false && boo_pineapple == false && boo_peppers == true) {
+                    v.setImageResource(R.drawable.pepperoni_pepper);
+                }
+
+                else if (boo_pepperoni == false && boo_onions == true && boo_pineapple == true && boo_peppers == false) {
+                    v.setImageResource(R.drawable.onions_pine);
+                }
+
+                else if (boo_pepperoni == false && boo_onions == true && boo_pineapple == false && boo_peppers == true) {
+                    v.setImageResource(R.drawable.onion_pepper);
+                }
+
+                else if (boo_pepperoni == false && boo_onions == false && boo_pineapple == true && boo_peppers == true) {
+                    v.setImageResource(R.drawable.pine_pepper);
+                }
+
+                else if (boo_pepperoni == true && boo_onions == false && boo_pineapple == false && boo_peppers == false) {
+                    v.setImageResource(R.drawable.pepperoni_only);
+                }
+
+                else if (boo_pepperoni == false && boo_onions == true && boo_pineapple == false && boo_peppers == false) {
+                    v.setImageResource(R.drawable.onions_only);
+                }
+
+                else if (boo_pepperoni == false && boo_onions == false && boo_pineapple == true && boo_peppers == false) {
+                    v.setImageResource(R.drawable.pineapple_only);
+                }
+
+                else if (boo_pepperoni == false && boo_onions == false && boo_pineapple == false && boo_peppers == true) {
+                    v.setImageResource(R.drawable.peppers_only);
+                }
+
+                else if (boo_pepperoni == false && boo_onions == false && boo_pineapple == false && boo_peppers == false && type.equals("MEAT")) {
                     v.setImageResource(R.drawable.meat);
                 }
 
-                //if(boo_pepperoni==true) v.setImageResource(R.drawable.meat);
-                //if(boo_onions==true) {v.setImageResource(R.drawable.cheese);}
-                //if(boo_pineapple==true) {v.setImageResource(R.drawable.cheese);}
-                //if(boo_peppers==true) {v.setImageResource(R.drawable.bbq);}
-                //if(boo_pepperoni==true && boo_onions==true  ) v.setImageResource(R.drawable.bbq);
+                else if (boo_pepperoni == false && boo_onions == false && boo_pineapple == false && boo_peppers == false && type.equals("BBQ")) {
+                    v.setImageResource(R.drawable.bbq);
+                }
+
+                else if (boo_pepperoni == false && boo_onions == false && boo_pineapple == false && boo_peppers == false && type.equals("CHEESE")) {
+                    v.setImageResource(R.drawable.cheese);
+                }
+
+                else if (boo_pepperoni == false && boo_onions == false && boo_pineapple == false && boo_peppers == false && type.equals("CUSTOM")) {
+                    v.setImageResource(R.drawable.custom);
+                }
+
+
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(Customize.this,Menu.class);
+                startActivity(intent);
+            }
+        });
+
+        total_price=unit_p + (counter*2.0);
+
+        payment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2= new Intent(Customize.this,Payment.class);
+                intent2.putExtra("TOATL_PRICE",Double.toString(total_price));
+                startActivity(intent2);
             }
         });
 
